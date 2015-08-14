49e6c63cf316d4a95896d3fc6e0ab6a6ec3a0e1a
Jess Smith <smith.jessk@gmail.com>
2015-08-14 17:06:17 -0400

    Add LocalScheduler to MedianPolish

--- bio/GISTs/medianPolish.h.php
+++ bio/GISTs/medianPolish.h.php
@@ -1,7 +1,7 @@
 <?
 function Median_Polish($t_args, $outputs, $states) {
     $className = generate_name('MP');
-    $output = ['polished_matrix' => lookupType('matrix')];
+    $output = ['polished_matrix' => lookupType('Variable_Matrix')];
 
     $identifier = [
         'kind'  => 'GIST',
@@ -17,11 +17,11 @@ function Median_Polish($t_args, $outputs, $states) {
 
 class ConvergenceGLA {
  public:
-  int roundNumber;
+  int roundNum;
   bool convergingThisRound;
 
   ConvergenceGLA(int num) :
-      roundNumber(num),
+      roundNum(num),
       convergingThisRound(true) {}
 
   void AddState(ConvergenceGLA other) {
@@ -29,7 +29,7 @@ class ConvergenceGLA {
   }
 
   bool ShouldIterate() {
-    bool done = (roundNumber > 1) && convergingThisRound;
+    bool done = (roundNum > 1) && convergingThisRound;
     return !done;
   }
 };
@@ -37,34 +37,99 @@ class ConvergenceGLA {
 class <?=$className?> {
  public:
   struct Task {
-    int index; // Which row or column this local scheduler starts at
-    int width; // How many rows / cols to be assigned to this LS
+    long startIndex; // Which row or column this local scheduler starts at
+    long endIndex; // Which row/col it ends at. Inclusive bound.
   };
 
-  struct SingleThreadScheduler {
+  struct LocalScheduler {
     int threadIndex;
-    bool scheduledTask;
+    bool finishedScheduling;
 
-    SingleThreadScheduler(int index) :
+    LocalScheduler(int index) :
         threadIndex(index),
         scheduledTask(false) {}
 
     bool GetNextTask(Task& task) {
-      bool ret = !finished;
-      printf("Getting task from scheduler %d: %d\n", index, ret);
-      task.index = index;
-      finished = true;
+      bool ret = !finishedScheduling;
+      long count;
+      if (roundNum % 2 == 1) {
+        count = matrix.n_rows;
+      } else {
+        count = matrix.n_cols;
+      }
+      task.startIndex = threadIndex * count / numThreads;
+      task.endIndex = (threadIndex + 1) * count / (numThreads - 1);
+      finishedScheduling = true;
       return ret;
     }
   };
 
-  // Starts at 1. Without a hard limit on the number of iterations, median 
-  // polishing could go on forever.
-  int roundNumber;
+  struct Iterator {
+    // The fragment ID for this tree, corresponding to the tree index.
+    long fragmentID;
 
+    // The index for the current output of this fragment.
+    long fragmentIndex;
+  };
+
+  // We don't know what type of matrix we will be passed, so it is best to be
+  // type-agnostic.
+  using Matrix = <?=$states[0]?>::Matrix;
   using cGLA = ConvergenceGLA;
   using WorkUnit = pair<LocalScheduler*, cGLA*>;
   using WorkUnits = vector<WorkUnit>;
+
+ private:
+  // Without a limit on the number of iterations, the process could never end.
+  int roundNum;
+  int numThreads;
+  Matrix matrix;
+
+  void RowPolish(Task& task, cGLA& gla) {
+    int start = task.startIndex;
+    int end = task.endIndex;
+    auto medians = median(matrix.submat(start, 0, end, matrix.n_cols - 1), 1);
+    matrix.submat(start, 0, end, matrix.n_cols - 1) - medians;
+  }
+
+  void ColPolish(Task& task, cGLA& gla) {
+    int start = task.startIndex;
+    int end = task.endIndex;
+    auto medians = median(matrix.submat(0, start, matrix.n_rows - 1, end), 0);
+    matrix.submat(start, 0, end, matrix.n_cols - 1) - medians;
+  }
+
+ public:
+  <?=$className?>(<?=const_typed_ref_args($states_)?>):
+      roundNum(0) {
+    cout << "Constructed GIST state" << endl;
+  }
+
+  // Advance the round number and distribute work among the threads
+  void PrepareRound(WorkUnits& workers, int numThreads) {
+    roundNum++;
+    this->numThreads = numThreads;
+    cout << "Beginning round " << roundNum << " with " << numThreads
+      << " workers." << endl;
+    for (int counter = 0; counter < numThreads; counter++) {
+      workers.push_back(new LocalScheduler(counter), new cGLA(roundNum));
+    }
+  }
+
+  // If round number is odd, do a row polish. Otherwise, do a column polish.
+  void DoStep(Task& task, cGLA& gla) {
+    if (roundNum % 2 == 1) {
+      RowPolish(task, gla);
+    } else {
+      ColPolish(task, gla);
+    }
+  }
+
+  int GetNumFragments() {}
+
+  Iterator* Finalize(long fragment) {}
+
+  bool GetNextResult(Iterator* it, <?=typed_ref_args($outputs_)?>) {}
 }
 
 <?
