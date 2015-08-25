a5f884e1467e747e20c0b08498908c7439069c58
Jess Smith <smith.jessk@gmail.com>
2015-08-25 15:23:32 -0400

    Change name of parameter

--- gtBio/R/input.R
+++ gtBio/R/input.R
@@ -1,10 +1,12 @@
-ReadCEL <- function(files, outputs, ...) {
+ReadCEL <- function(paths, outputs, ...) {
   if (missing(outputs)) {
     outputs <- c("Intensity", "StdDev", "Pixels")
   }
 
+  # Expand directories to their lists of files
+
   alias <- create.alias("read")
   gi <- GI(bio::CELFileReader)
-  data <- Input(files = files, alias = alias, gi = gi, schema = outputs, ...)
+  data <- Input(files = paths, alias = alias, gi = gi, schema = outputs, ...)
   set.class(c(data), c("ReadFile", class(data)))
 }
