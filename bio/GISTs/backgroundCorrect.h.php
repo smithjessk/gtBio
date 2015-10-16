e197d3904880ef2f2a7d8289682ab07c5ab0e5a1
Jess Smith <smith.jessk@gmail.com>
2015-10-16 12:07:25 -0400

    Add code for background polish DoStep and change casing to underscores

--- bio/GISTs/backgroundCorrect.h.php
+++ bio/GISTs/backgroundCorrect.h.php
@@ -1,80 +1,73 @@
 <?
 function Background_Correct(array $t_args, array $inputs, array $outputs) {
-  $className = generate_name('Background_Correct');
+  $class_name = generate_name('Background_Correct');
   $matrix = array_keys($states)[0];
-  $matrixType = array_values($states)[0];
-  $innerType = $matrixType->get('type');
-  $shouldTranspose = get_default($t_args, 'shouldTranspose', False);
-  $fieldToAccess = get_default($t_args, 'fieldToAccess', '');
-  if ($fieldToAccess != '') {
-    $fieldToAccess = '.' + $fieldToAccess;
+  $matrix_type = array_values($states)[0];
+  $inner_type = $matrix_type->get('type');
+  $should_transpose = get_default($t_args, 'should_transpose', False);
+  $field_to_access = get_default($t_args, 'field_to_access', '');
+  if ($field_to_access != '') {
+    $field_to_access = '.' + $field_to_access;
   }
   $output = ['corrected_matrix' => lookupType('bio::Variable_Matrix', 
-      ['type' => $innerType])];
+      ['type' => $inner_type])];
   
   $identifier = [
       'kind' => 'GIST',
-      'name' => $className,
+      'name' => $class_name,
       'system_headers' => ['armadillo', 'algorithm'],
       'libraries'     => ['armadillo', 'preprocessCore'],
       'user_headers'    => [],
-      'iterable' => true,
+      'iterable' => false,
       'output'          => $output,
       'result_type'     => 'single',
   ];
 ?>
 
 // Found in preprocessCore
-extern void rma_bg_parameters(double *PM,double *param, size_t rows, 
+extern void rma_bg_parameters(double *PM, double *param, size_t rows, 
   size_t cols, size_t column);
 extern void rma_bg_adjust(double *PM, double *param, size_t rows, size_t cols, 
   size_t column);
 
 class ConvergenceGLA {
  public:
-  int roundNum;
-
-  ConvergenceGLA(int num) :
-      roundNum(num) {}
+  ConvergenceGLA() {}
 
   void AddState(ConvergenceGLA other) {}
-
-  // TODO: Add better converging conditions
-  bool ShouldIterate() {
-    return roundNum < 2;
-  }
 };
 
-class <?=$className?> {
+class <?=$class_name?> {
  public:
   // We don't know what type of matrix we will be passed, so it is best to be
   // type-agnostic.
-  using InnerType = <?=$innerType?>;
-  using Matrix = <?=$matrixType?>::Matrix;
+  using InnerType = <?=$inner_type?>;
+  using Matrix = <?=$matrix_type?>::Matrix;
   using cGLA = ConvergenceGLA;
 
   struct Task {
-    long colIndex;
+    long col_index;
   };
 
   struct LocalScheduler {
-    int threadIndex;
-    bool finishedScheduling;
-    int numThreads;
-    int &roundNum;
+    int thread_index;
+    bool finished_scheduling;
+    int num_threads;
+    int &round_num;
     Matrix &matrix;
 
-    LocalScheduler(int index, int &roundNum, int numThreads, Matrix &matrix) :
-        threadIndex(index),
-        finishedScheduling(false),
-        numThreads(numThreads),
-        roundNum(roundNum),
+    LocalScheduler(int index, int &round_num, int num_threads, 
+      Matrix &matrix) :
+        thread_index(index),
+        finished_scheduling(false),
+        num_threads(num_threads),
+        round_num(round_num),
         matrix(matrix) {}
 
     bool GetNextTask(Task& task) {
-      bool ret = !finishedScheduling;
-      // task.colIndex = threadIndex;
-      finishedScheduling = true;
+      bool ret = !finished_scheduling;
+      // task.col_index = thread_index;
+      finished_scheduling = true;
       return ret;
     }
   };
@@ -83,43 +76,47 @@ class <?=$className?> {
   using WorkUnits = std::vector<WorkUnit>;
 
  private:
-  int roundNum;
-  int numThreads;
+  int round_num;
+  int num_threads;
   Matrix matrix;
 
  public:
-  <?=$className?>(<?=const_typed_ref_args($states)?>) {
+  <?=$class_name?>(<?=const_typed_ref_args($states)?>) {
 
-<? if ($shouldTranspose) { ?>
+<? if ($should_transpose) { ?>
         std::cout << "Transposing matrix..." << std::endl;
-        matrix = <?=$matrix?>.GetMatrix()<?=$fieldToAccess?>.t();
+        matrix = <?=$matrix?>.GetMatrix()<?=$field_to_access?>.t();
 <? } else { ?>
-        matrix = <?=$matrix?>.GetMatrix()<?=$fieldToAccess?>;
+        matrix = <?=$matrix?>.GetMatrix()<?=$field_to_access?>;
 <? } ?> 
-        roundNum = 0;
+        round_num = 0;
   }
 
   void PrepareRound(WorkUnits& workers, int suggested_num_workers) {
-    roundNum++;
-    this->numThreads = matrix.n_cols;
-    std::cout << "Beginning round " << roundNum << " with " << this->numThreads
+    round_num++;
+    this->num_threads = std::min(matrix.n_cols, suggested_num_workers);
+    std::cout << "Beginning round " << round_num << " with " << this->num_threads
       << " workers." << std::endl;
     std::pair<LocalScheduler*, cGLA*> worker;
-    for (int counter = 0; counter < this->numThreads; counter++) {
-      worker = std::make_pair(new LocalScheduler(counter, roundNum, 
-        this->numThreads, matrix), new cGLA(roundNum));
+    for (int counter = 0; counter < this->num_threads; counter++) {
+      worker = std::make_pair(new LocalScheduler(counter, round_num, 
+        this->num_threads, matrix), new cGLA());
       workers.push_back(worker);
     }
   }
 
+  // TODO: How to extract a pointer to the data? 
   void DoStep(Task& task, cGLA& gla) {
-    // Here go the equivalent calls to rma_bg_correct and rma_bg_adjust
-    // Note that this should mirror the code here 
-    // https://github.com/Bioconductor-mirror/preprocessCore/blob/master/src/rma_background4.c#L444
+    matrix = matrix.t();
+    double *params = malloc(3 * sizeof(double));
+    rma_bg_parameters(matrix.memptr(), params, matrix.n_rows, matrix.n_cols, 
+      (size_t) task.col_index);
+    rma_bg_adjust(matrix.memptr(), params, matrix.n_rows, matrix.n_cols, 
+      (size_t) task.col_index);
   }
 
   void GetResult(<?=typed_ref_args($output)?>) {
-    corrected_matrix = matrix;
+    corrected_matrix = matrix.t();
   }
 }
 
