c04ddce834ae15993263dd77bb9de5de1ea36925
Jess Smith <smith.jessk@gmail.com>
2015-10-20 16:22:55 -0400

    Fix template argument name

--- examples/cmpQuery.R
+++ examples/cmpQuery.R
@@ -3,6 +3,7 @@ library(gtBase)
 
 f <- "../demoData/command-console/GSM1134065_GBX.DISC.PCA2.CEL"
 data <- ReadCEL(c(f))
-normalizedMatrix <- MedianPolish(states = data, outputs = 
-  c(normalized = Matrix), shouldTranspose = FALSE, fieldToAccess = "Intensity")
+state <- Gather(data)
+normalizedMatrix <- MedianPolish(states = state, outputs = 
+  c(normalized = Matrix), shouldTranspose = FALSE, field_to_access = "Intensity")
 x <- View(normalizedMatrix)
