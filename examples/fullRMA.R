5e2f71ed14ae07087c9bddc87d67460241ecaafb
Jess Smith <smith.jessk@gmail.com>
2015-10-22 15:30:53 -0400

    Rewrite RMA function to be more intuitive and documented. Update examples accordingly

--- examples/fullRMA.R
+++ examples/fullRMA.R
@@ -8,14 +8,5 @@ library(gtBase)
 # state <- Collect(data, c(Row, Data), Matrix, size = 5)
 
 f <- "../demoData/command-console/GSM1134065_GBX.DISC.PCA2.CEL"
-data <- ReadCEL(c(f))
-state <- Gather(data)
-
-correctedMatrix <- BackgroundCorrect(states = state, outputs = 
-  c(corrected = Matrix), shouldTranspose = FALSE, 
-  field_to_access = "Q1__Intensity")
-
-normalizedMatrix <- MedianPolish(states = correctedMatrix, outputs = 
-  c(normalized = Matrix), shouldTranspose = FALSE)
-
+normalizedMatrix <- RMA(f)
 x <- View(normalizedMatrix)
--- gtBio/R/normalize.R
+++ gtBio/R/normalize.R
@@ -1,8 +1,22 @@
-RMA <- function(data, ...) {
-  # For a reference, look at just.rma and rma_c_call in the affy package
-  data <- BackgroundCorrect(data, ...)
-  # Group normalization (and the model stuff)
-  # Median Polish
+# For a reference, see the following links:
+# https://github.com/Bioconductor-mirror/affy/blob/master/R/justrma.R#L55
+# https://github.com/Bioconductor-mirror/affy/blob/4a732dd80fabbc8e7b008428dfdea57af6bd237b/src/rma2.c#L163
+RMA <- function(files) {
+  data <- ReadCEL(c(files))
+  state <- Gather(data)
+
+  # This is equivalent to rma_bg_correct in just.rma
+  correctedMatrix <- BackgroundCorrect(states = state, outputs = 
+    c(corrected = Matrix), shouldTranspose = FALSE, 
+    field_to_access = "Q1__Intensity")
+
+  # The following code are equivalent to rma_c_call in just.rma
+  
+  # qnorm_c on perfect match pairs
+
+  # Equivalent to R_subColSummarize_medianpolish_log
+  MedianPolish(states = correctedMatrix, outputs = 
+    c(normalized = Matrix), shouldTranspose = FALSE)
 }
 
 BackgroundCorrect <- function(outputs, states, ...) {
