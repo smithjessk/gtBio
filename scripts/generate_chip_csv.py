e2d7fe3dface5936cc356104f99a811793f0b035
Jess Smith <smith.jessk@gmail.com>
2015-12-31 12:57:47 -0500

    Add CSV writer for SQL database

--- scripts/generate_chip_csv.py
+++ scripts/generate_chip_csv.py
@@ -1,40 +1,47 @@
 import sqlite3
 import sys
 import getopt
+import csv
+import os
 
 def usage():
-    print "Usage: " + sys.argv[0] + "-f <path_to_sqlite_file>"
+    print "Usage: " + sys.argv[0] + "-f <path_to_sqlite_file> -o <path_to_output_file>"
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hf:", ["help", "file="])
+        opts, args = getopt.getopt(sys.argv[1:], "hf:o:", ["help", "file=", "output="])
     except getopt.GetoptError as err:
         print str(err)
         usage()
         sys.exit(2)
-    file = None
+    file_path = None
+    output_path = None
     for option, argument in opts:
         if option in ("-h", "--help"):
             usage()
             sys.exit()
         elif option in ("-f", "--file"):
-            file = argument
+            file_path = argument
+        elif option in ("-o", "--output"):
+            output_path = argument
         else:
             assert False, "Unhandled option"
-    generate_csv(file)
+    generate_csv(file_path, output_path)
 
-
-def generate_csv(file):
-    if (file == None):
+def generate_csv(file_path, output_path):
+    if (file_path == None or output_path == None):
         usage()
         sys.exit(2)
-    conn = sqlite3.connect(file)
+    conn = sqlite3.connect(file_path)
     c = conn.cursor()
     statement = "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN " + "core_mps USING(fsetid)"
     num_row = 0
-    for row in c.execute(statement):
-        num_row = num_row + 1
-    print(num_row)
+    with open(output_path, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["fid", "fsetid"])
+        for row in c.execute(statement):
+            csv_writer.writerow(row)
+    print "Wrote output to " + output_path
 
 if __name__ == "__main__":
     main()
