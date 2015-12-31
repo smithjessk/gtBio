a2b195aa5518ea849a906fcfc26da496d2c25d31
Jess Smith <smith.jessk@gmail.com>
2015-12-31 14:05:47 -0500

    Use multiple data types in examples

--- examples/extract_perfect_match.R
+++ examples/extract_perfect_match.R
@@ -3,9 +3,10 @@ library(gtBase)
 
 celFile1 <- "../demoData/command-console/GSM1134065_GBX.DISC.PCA2.CEL"
 celFile2 <- "../demoData/command-console/GSM1134066_GBX.DISC.PCA3.CEL"
+celFile3 <- "../demoData/xda/GSM1134065_GBX.DISC.PCA2.CEL"
 infoFile <- "../scripts/pd.huex.1.0.st.v2.csv"
 
-data <- ReadCEL(c(celFile1, celFile2))
+data <- ReadCEL(c(celFile1, celFile2, celFile3))
 info <- ReadPMInfoFile(infoFile)
 joined <- Join(data, fid, info, fid)
 x <- View(Count(joined))
