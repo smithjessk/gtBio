9209517041058646c5c590e5d68205849a23c3ed
Jess Smith <smith.jessk@gmail.com>
2016-01-29 10:42:56 -0500

    Add median polish GLA

---
+++ bio/GLAs/medianPolish.h.php
@@ -0,0 +1,106 @@
+<?
+function Median_Polish(array $t_args, array $inputs, array $outputs,
+    array $states) {
+    $className = generate_name('Median_Polish');
+    $inputs_ = array_combine(['file_name', 'ordered_fid', 'ordered_fsetid',
+      'fid', 'fsetid', 'intensity'], $inputs);
+    $output_types = [lookupType('base::string'), lookupType('fsetid'),
+      lookupType('float')];
+    $outputs_ = array_combine(['file_name', 'intensity', 'fsetid'],
+      $output_types);
+
+    $sys_headers  = ['armadillo', 'unordered_map'];
+    $user_headers = [];
+    $lib_headers  = [];
+    $libraries    = ['armadillo'];
+    $properties   = ['matrix', 'tuples'];
+    $extra        = [];
+    $identifier = [
+        'kind'              => 'GLA',
+        'name'              => $className,
+        'system_headers'    => $sys_headers,
+        'user_headers'      => $user_headers,
+        'lib_headers'       => $lib_headers,
+        'libraries'         => $libraries,
+        'iterable'          => false,
+        'input'             => $inputs,
+        'output'            => $output_type,
+        'result_type'       => 'fragment',
+        'finalize_as_state' => true,
+        'properties'        => $properties,
+        'extra'             => $extra,
+    ];
+?>
+
+class <?=$className?>;
+
+class <?=$className?> {
+ private:
+  // Columns are files, rows are probes in this probeset
+  arma::Mat<float> probeset_matrix;
+  int num_probes_encountered;
+  int num_produced;
+  std::string fsetid;
+
+  void resize_matrix(int num_rows) {
+    int old_num_rows = probeset_matrix.n_rows;
+    probeset_matrix = arma::resize(probeset_matrix, num_rows, <?=$num_files?>);
+    for (size_t index = old_num_rows; index < num_rows; index++) {
+      probeset_matrix.row(index).fill(0);
+    }
+  }
+
+ public:
+  <?=$className?>()
+    : probeset_matrix(50, <?=$num_files?>),
+    num_probes_encountered(0),
+    num_produced(0) {
+      probeset_matrix.fill(0);
+    }
+  }
+
+  arma::Mat<float> &get_probeset_matrix() {
+    return *(this->probeset_matrix);
+  }
+
+  void AddItem(<?=const_typed_ref_args($inputs_)?>) {
+    this->fsetid = fsetid;
+    num_probes_encountered++;
+    if (num_probes_encountered > probeset_matrix.n_rows) {
+      resize_matrix(1.2 * probeset_matrix.n_rows, <?=$num_files?>);
+    }
+    int col_index = get_col(file_name.ToString());
+    probeset_matrix(ordered_fsetid, col_index) = intensity;
+  }
+
+  void AddState(<?=$className?> &other) {
+    this->probeset_matrix += other.get_probeset_matrix();
+  }
+
+
+  void FinalizeState() {
+    resize_matrix(num_probes_encountered, <?=$num_files?>);
+    probeset_matrix = log2(probeset_matrix);
+    double *results = (double *) malloc(sizeof(double) * <?=$num_files?>);
+    double *resultsSE = (double *) malloc(sizeof(double) * <?=$num_files?>);
+    median_polish_no_copy(probeset_matrix.memptr(), num_probes_encountered, 
+      <?=$num_files?>, results, resultsSE);
+    std::cout << "<?=$className?> finished" << std::endl;
+  }
+
+  bool GetNextResult(<?=typed_ref_args($outputs_)?>) const {
+    if (num_produced == <?=$num_files?>) {
+      return false;
+    }
+    file_name = files_names.at(num_produced);
+    intensity = intensities.at(num_produced);
+    fsetid = this->fsetid;
+    return true;
+  }
+};
+
+<?
+    $identifier['generated_state'] = $constantState;
+    return $identifier;
+}
+?>
